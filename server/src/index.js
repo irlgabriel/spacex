874f76b6d963caf4bcb711ab83b318c2f9a18507
irlgabriel <radu.gabriel131@gmail.com>
2020-10-16 19:56:54 +0300

    added schema

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,10 @@
 require('dotenv').config();
+
+import { ApolloServer } from "apollo-server"
+import { typeDefs } from "./schema"
+
+const server = new ApolloServer({ typeDefs })
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
